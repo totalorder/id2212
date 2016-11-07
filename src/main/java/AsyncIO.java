53e1a0a16a2d011ccda17c0577008619b7f26a77
Anton Blomberg <anton@deadlock.se>
2016-11-07 18:02:09 +0100

    Implement sending in Client

--- src/main/java/AsyncIO.java
+++ src/main/java/AsyncIO.java
@@ -4,10 +4,8 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
+import java.util.NoSuchElementException;
+import java.util.concurrent.*;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
@@ -30,14 +28,32 @@ public class AsyncIO implements Closeable {
   }
 
   class Client {
+    class SendBuffer {
+      private final CompletableFuture<Void> sentFuture;
+      private final ByteBuffer byteBuffer;
+
+      private SendBuffer(final CompletableFuture<Void> sentFuture,
+                         final ByteBuffer byteBuffer) {
+        this.sentFuture = sentFuture;
+        this.byteBuffer = byteBuffer;
+      }
+    }
+
     private final SocketChannel channel;
     private final ByteBuffer buffer;
     private int lastPosition = 0;
+    private ConcurrentLinkedDeque<SendBuffer> sendBuffers = new ConcurrentLinkedDeque<>();
+    private volatile SendBuffer currentlyWriting = null;
 
+    /**
+     * NOTE: Client is currently not thread safe. onIsReadable() and onIsWritable() is expected
+     * to be called from only one thread. send() however is thread-safe.
+     *
+     * @param channel
+     */
     public Client(final SocketChannel channel) {
       this.channel = channel;
       buffer = ByteBuffer.allocate(1024);
-
     }
 
     protected void onIsReadable() throws IOException {
@@ -59,6 +75,37 @@ public class AsyncIO implements Closeable {
         buffer.clear();
       }
     }
+
+    protected void onIsWritable() throws IOException {
+      if (currentlyWriting == null && !sendBuffers.isEmpty()) {
+        try {
+          currentlyWriting = sendBuffers.removeFirst();
+        } catch (NoSuchElementException e) {
+          // It's empty, do nothing
+        }
+      }
+
+      if (currentlyWriting != null) {
+        while (currentlyWriting.byteBuffer.hasRemaining() &&
+            channel.write(currentlyWriting.byteBuffer) > 0) {
+        }
+
+        if (!currentlyWriting.byteBuffer.hasRemaining()) {
+          currentlyWriting.sentFuture.complete(null);
+          currentlyWriting = null;
+        }
+      }
+    }
+
+    public CompletionStage<Void> send(final byte[] bytes) {
+      return send(ByteBuffer.wrap(bytes));
+    }
+
+    public CompletionStage<Void> send(final ByteBuffer byteBuffer) {
+      final CompletableFuture<Void> sentFuture = new CompletableFuture<>();
+      sendBuffers.addLast(new SendBuffer(sentFuture, byteBuffer));
+      return sentFuture;
+    }
   }
 
   public AsyncIO(final Consumer<Client> clientAcceptedCallback,
@@ -79,8 +126,14 @@ public class AsyncIO implements Closeable {
           for (final SelectionKey key : selector.selectedKeys()) {
             if (key.isAcceptable()) {
               accept(key.channel());
-            } else if (key.isReadable()) {
+            }
+
+            if (key.isReadable()) {
               ((Client)key.attachment()).onIsReadable();
+            }
+
+            if (key.isWritable()) {
+              ((Client)key.attachment()).onIsWritable();
             }
           }
           selector.selectedKeys().clear();
@@ -103,12 +156,18 @@ public class AsyncIO implements Closeable {
 
   private void accept(final SelectableChannel acceptableChannel) throws IOException {
     final SocketChannel clientChannel = ((ServerSocketChannel)acceptableChannel).accept();
+
+    final Client client = createClient(clientChannel);
+
+    clientAcceptedCallback.accept(client);
+  }
+
+  private Client createClient(final SocketChannel clientChannel) throws IOException {
     final Client client = new Client(clientChannel);
 
     clientChannel.configureBlocking(false);
     clientChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE, client);
-
-    clientAcceptedCallback.accept(client);
+    return client;
   }
 
 
@@ -133,6 +192,36 @@ public class AsyncIO implements Closeable {
     selectForever();
 
     return started;
+  }
+
+  public CompletionStage<Client> connect(final InetSocketAddress address) throws IOException {
+    final SocketChannel channel = SocketChannel.open();
+    channel.configureBlocking(true);
+    channel.connect(address);
+    final CompletableFuture<Void> clientConnectedFuture = new CompletableFuture<>();
+    awaitConnection(channel, clientConnectedFuture);
+    return clientConnectedFuture.thenApply(ignored -> {
+      try {
+        return createClient(channel);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    });
+  }
+
+  private void awaitConnection(final SocketChannel channel,
+                               final CompletableFuture<Void> connectedFuture) {
+    ForkJoinPool.commonPool().execute(() -> {
+      try {
+        if (channel.finishConnect()) {
+          connectedFuture.complete(null);
+        } else {
+          awaitConnection(channel, connectedFuture);
+        }
+      } catch (IOException e) {
+        connectedFuture.completeExceptionally(e);
+      }
+    });
   }
 
   public int getListeningPort() {
--- src/test/java/AsyncIOTest.java
+++ src/test/java/AsyncIOTest.java
@@ -8,7 +8,10 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -16,14 +19,14 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 
 public class AsyncIOTest {
 
   private List<AsyncIO.Client> acceptedClients = new ArrayList<>();
-  private List<AsyncIO.Client> dataReceivedClients = new ArrayList<>();
-  private List<ByteBuffer> dataReceivedBuffers = new ArrayList<>();
+  private Map<AsyncIO.Client, List<ByteBuffer>> dataReceivedBuffers = new HashMap<>();
   private AsyncIO asyncIO;
   private final CountDownLatch acceptLatch = new CountDownLatch(1);
   private CountDownLatch dataReceivedLatch = new CountDownLatch(1);
@@ -35,13 +38,15 @@ public class AsyncIOTest {
       acceptLatch.countDown();
     };
 
-    final BiConsumer<AsyncIO.Client, ByteBuffer> dataRecievedCallback = (client, byteBuffer) -> {
-      dataReceivedClients.add(client);
-      dataReceivedBuffers.add(byteBuffer);
+    final BiConsumer<AsyncIO.Client, ByteBuffer> dataReceivedCallback = (client, byteBuffer) -> {
+      if (!dataReceivedBuffers.containsKey(client)) {
+        dataReceivedBuffers.put(client, new ArrayList<>());
+      }
+      dataReceivedBuffers.get(client).add(byteBuffer);
       dataReceivedLatch.countDown();
     };
 
-    asyncIO = new AsyncIO(clientAcceptedCallback, dataRecievedCallback);
+    asyncIO = new AsyncIO(clientAcceptedCallback, dataReceivedCallback);
   }
 
   @After
@@ -81,8 +86,8 @@ public class AsyncIOTest {
     // Then
     assertEquals(9, written);
     dataReceivedLatch.await(1, TimeUnit.SECONDS);
-    assertEquals(1, dataReceivedClients.size());
-    final ByteBuffer byteBuffer = dataReceivedBuffers.get(0);
+    assertEquals(1, dataReceivedBuffers.size());
+    final ByteBuffer byteBuffer = dataReceivedBuffers.entrySet().iterator().next().getValue().get(0);
     assertEquals("Test data", stringFromUTF8Buffer(byteBuffer));
   }
 
@@ -98,7 +103,7 @@ public class AsyncIOTest {
     // Write + wait for receive
     channel.write(ByteBuffer.wrap("Test data".getBytes("UTF-8")));
     dataReceivedLatch.await(1, TimeUnit.SECONDS);
-    final ByteBuffer byteBuffer = dataReceivedBuffers.get(0);
+    final ByteBuffer byteBuffer = dataReceivedBuffers.entrySet().iterator().next().getValue().get(0);
     assertEquals("Test data", stringFromUTF8Buffer(byteBuffer));
 
     dataReceivedLatch = new CountDownLatch(1);
@@ -109,9 +114,82 @@ public class AsyncIOTest {
     // Then
     assertEquals(11, written);
     dataReceivedLatch.await(1, TimeUnit.SECONDS);
-    assertEquals(2, dataReceivedClients.size());
-    final ByteBuffer byteBuffer2 = dataReceivedBuffers.get(1);
+    assertEquals(2, dataReceivedBuffers.entrySet().iterator().next().getValue().size());
+    final ByteBuffer byteBuffer2 = dataReceivedBuffers.entrySet().iterator().next().getValue().get(1);
     assertEquals("Test data 2", stringFromUTF8Buffer(byteBuffer2));
+  }
+
+  @Test
+  public void canConnect() throws ExecutionException, InterruptedException, IOException {
+    // Given
+    asyncIO.startServer(0).toCompletableFuture().get();
+
+    // When
+    AsyncIO.Client client = asyncIO.connect(new InetSocketAddress(asyncIO.getListeningPort()))
+        .toCompletableFuture().get();
+
+    // Then
+    assertNotNull(client);
+    acceptLatch.await(1, TimeUnit.SECONDS);
+    assertEquals(1, acceptedClients.size());
+  }
+
+  @Test
+  public void canSend() throws ExecutionException, InterruptedException, IOException {
+    // Given
+    // Connect + wait for accept
+    asyncIO.startServer(0).toCompletableFuture().get();
+    AsyncIO.Client client = asyncIO.connect(new InetSocketAddress(asyncIO.getListeningPort()))
+        .toCompletableFuture().get();
+    acceptLatch.await(1, TimeUnit.SECONDS);
+
+    // When
+    final CompletionStage<Void> sentFuture = client.send("Test data".getBytes("UTF-8"));
+
+    // Then
+    dataReceivedLatch.await(1, TimeUnit.SECONDS);
+    sentFuture.toCompletableFuture().get();
+    assertEquals(1, dataReceivedBuffers.entrySet().iterator().next().getValue().size());
+    final ByteBuffer byteBuffer2 = dataReceivedBuffers.entrySet().iterator().next().getValue().get(0);
+    assertEquals("Test data", stringFromUTF8Buffer(byteBuffer2));
+  }
+
+  @Test
+  public void canSendAndReceiveInterleavedMessages() throws ExecutionException, InterruptedException, IOException {
+    // Given
+    // Connect + wait for accept
+    asyncIO.startServer(0).toCompletableFuture().get();
+    AsyncIO.Client connectedClient = asyncIO.connect(new InetSocketAddress(asyncIO.getListeningPort()))
+        .toCompletableFuture().get();
+    acceptLatch.await(1, TimeUnit.SECONDS);
+    AsyncIO.Client acceptedClient = acceptedClients.get(0);
+
+    // Connected writes C --> A
+    connectedClient.send("Test data".getBytes("UTF-8")).toCompletableFuture().get();
+
+    // Accepted reads A <-- C
+    dataReceivedLatch.await(1, TimeUnit.SECONDS);
+    final ByteBuffer byteBuffer = dataReceivedBuffers.get(acceptedClient).get(0);
+    assertEquals("Test data", stringFromUTF8Buffer(byteBuffer));
+
+    // Accepted writes A --> C
+    dataReceivedLatch = new CountDownLatch(1);
+    acceptedClient.send("Test data 2".getBytes("UTF-8")).toCompletableFuture().get();
+
+    // Connected reads C <-- A
+    dataReceivedLatch.await(1, TimeUnit.SECONDS);
+    assertEquals(2, dataReceivedBuffers.entrySet().size());
+    final ByteBuffer byteBuffer2 = dataReceivedBuffers.get(connectedClient).get(0);
+    assertEquals("Test data 2", stringFromUTF8Buffer(byteBuffer2));
+
+    // Connected writes C --> A
+    dataReceivedLatch = new CountDownLatch(1);
+    connectedClient.send("Test data 3".getBytes("UTF-8")).toCompletableFuture().get();
+
+    // Accepted reads A <-- C
+    dataReceivedLatch.await(1, TimeUnit.SECONDS);
+    final ByteBuffer byteBuffer3 = dataReceivedBuffers.get(acceptedClient).get(1);
+    assertEquals("Test data 3", stringFromUTF8Buffer(byteBuffer3));
   }
 
   private String stringFromUTF8Buffer(ByteBuffer buffer) throws UnsupportedEncodingException {
